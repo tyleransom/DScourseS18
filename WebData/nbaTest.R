f4299b02b53e94e81eed88a6953f9dd83c2d7080
Tyler Ransom <tyleransom@gmail.com>
2018-02-13 13:18:27 -0600

    Updating NBA API notes

--- WebData/nbaTest.R
+++ WebData/nbaTest.R
@@ -4,10 +4,14 @@ library(dplyr)
 
 # Collect coaching staff info
 coaches <- get_coaching_staffs()
+print(coaches)
 
-filter(coaches, nameCollegeCoach == "Duke")
-filter(coaches, nameTeam == "San Antonio Spurs")
+print(filter(coaches, nameCollegeCoach == "Duke"))
+print(filter(coaches, nameTeam == "San Antonio Spurs"))
 
 thunder_contracts <- get_nba_team_salaries(team_name = "Oklahoma City Thunder", team_slug = NA)
+print(thunder_contracts)
 thunder_contracts$value <- thunder_contracts$value/1e6
+print(thunder_contracts$value)
 current_thunder <- filter(thunder_contracts,slugSeason=="2017-18") %>% select(slugSeason,nameTeam, namePlayer, isFinalSeason, value) 
+print(current_thunder)
