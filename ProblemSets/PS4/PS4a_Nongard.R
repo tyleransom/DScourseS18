e5134d7363a0b4cc1529898202999ab9de571c48
Econ Account <ouecon019@schooner2.oscer.ou.edu>
2018-02-12 19:14:52 -0600

    Completed PS4

---
+++ ProblemSets/PS4/PS4a_Nongard.R
@@ -0,0 +1,6 @@
+require("jsonlite")
+system('wget -O nflstats.json "http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2010&week=1&format=json"')
+system("cat nflstats.json")
+mydf<-fromJSON('nflstats.json')
+class(mydf$players)
+head(mydf,n=5)
