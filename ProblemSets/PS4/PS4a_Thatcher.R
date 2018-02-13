eb68b2bb1def11998d96d6f773b09252737ddff3
R-Thatcher <35077547+R-Thatcher@users.noreply.github.com>
2018-02-13 13:02:33 -0600

    Update PS4a_Thatcher.R

--- ProblemSets/PS4/PS4a_Thatcher.R
+++ ProblemSets/PS4/PS4a_Thatcher.R
@@ -1,5 +1,5 @@
 system('wget -O nflstats.json "http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2010&week=1&format=json"')
-system("cat nflstats.json")
+system('cat nflstats.json')
 install.packages("jsonlite")
 library("jsonlite")
 mydf <- fromJSON('nflstats.json')
