94e61027ded00b5c407f0f3c277ed0778c4b4822
Tyler Ransom <tyleransom@gmail.com>
2018-04-20 15:08:37 -0400

    Add Heckman selection example

--- Structural/example.R
+++ Structural/example.R
@@ -1,5 +1,8 @@
+#### Binary choice model
 library(mlogit)
-data(Heating) # load data on residential heating choice in CA
+
+# load data on residential heating choice in CA
+data(Heating) 
 levels(Heating$depvar) <- c("gas","gas","elec","elec","elec")
 
 # estimate logit and get predicted probabilities
@@ -19,3 +22,23 @@ print(summary(Heating$predProbit))
 estim$coefficients["income"] <- 4*estim$coefficients["income"]
 Heating$predLogitCfl <- predict(estim, newdata = Heating, type = "response")
 print(summary(Heating$predLogitCfl))
+
+
+
+#### Heckman selection model (from Garrett Glasgow's website)
+library(sampleSelection)
+
+# Load "Mroz" data on labor supply, fertility, and wages
+data("Mroz87")
+Mroz87$kids <- (Mroz87$kids5 + Mroz87$kids618 > 0)
+head(Mroz87)
+
+## Comparison of linear regression and selection model
+outcome1 <- lm(wage ~ exper, data = Mroz87)
+print(summary(outcome1))
+
+selection1 <- selection(selection = lfp ~ age + I(age^2) + faminc + kids + educ, outcome = wage ~ exper, 
+			data = Mroz87, method = "2step")
+print(summary(selection1))
+
+
